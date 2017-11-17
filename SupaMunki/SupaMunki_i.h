774698afc622d8ea804f0685d06d55b951056810
antbann <antbanspotter@hotmail.com>
2017-11-17 18:46:20 +0300

    add new functionality: SetIntValue, SetStringValue, SetFocus, Expand

--- SupaMunki/SupaMunki_i.h
+++ SupaMunki/SupaMunki_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Nov 17 12:52:50 2017
+/* at Fri Nov 17 18:08:43 2017
  */
 /* Compiler settings for SupaMunki.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
